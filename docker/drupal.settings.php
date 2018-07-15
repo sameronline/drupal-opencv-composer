a45db39af8c3a6dbc9b33daf29c1b01ad899ce96
Samer Ali <samer@sameronline.com>
2018-07-15 03:51:17 +0200

    Added new composer env-docker and composer env-opencv-dev for easier setup for docker env for dev

---
+++ docker/drupal.settings.php
@@ -0,0 +1,14 @@
+<?php
+
+$databases['default']['default'] = [
+    'host' => '{{ getenv "DB_HOST" "mariadb" }}',
+    'database' => '{{ getenv "DB_NAME" "drupal" }}',
+    'username' => '{{ getenv "DB_USER" "drupal" }}',
+    'password' => '{{ getenv "DB_PASSWORD" "drupal" }}',
+    'driver' => '{{ getenv "DB_DRIVER" "mysql" }}',
+    'prefix' => '{{ getenv "DB_PREFIX" }}',
+];
+$config_directories['sync'] = '../config/sync_dir';
+$settings['trusted_host_patterns'] = array(
+    {{ getenv "DRUPAL_TRUSTED_HOST_PATTERNS" "'\\.localhost$', '\\.local$', '\\.loc$'" }}
+);
